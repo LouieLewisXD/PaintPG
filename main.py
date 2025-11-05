7fff10b038209a94615b67567717668d6753dcbe
LucasJanssen <lucasjanssen2010@gmail.com>
2025-11-05 14:43:47 -0600

    Added

--- main.py
+++ main.py
@@ -10,27 +10,89 @@ WIN = py.display.set_mode((WIDTH, HEIGHT))
 
 clock = py.time.Clock()
 
-BLUE = (0, 0, 255)
+WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+BLACK = (0, 0, 0)
 
 class Shape():
     
-    def __init__(self,x,y,fill,border,thick):
-        self.x = x
-        self.y = y
+    def __init__(self,fill,border,border_color):
         self.fill = fill
         self.border = border
-        self.thick = thick
+        self.border_color = border_color
 
 class Rect(Shape):
     
+    def __init__(self,x,y,width,height,fill,border=0,border_color=0):
+        super().__init__(fill,border,border_color)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = py.rect.Rect(x,y,width,height)
+    
+    def draw(self):
+        if self.fill != None:
+            py.draw.rect(WIN,self.fill,self.rect)
+        
+        if self.border > 0:
+            py.draw.rect(WIN, self.border_color, self.rect, self.border)
+
+class Elipse(Shape):
+    def __init__(self,x,y,width,height,fill,border=0,border_color=0):
+        super().__init__(fill,border,border_color)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rect = py.rect.Rect(x,y,width,height)
+    
+    def draw(self):
+        if self.fill != None:
+            py.draw.ellipse(WIN,self.fill,self.rect)
+        
+        if self.border > 0:
+            py.draw.ellipse(WIN, self.border_color, self.rect, self.border)
+
+class Poly(Shape):
+    def __init__(self,points,fill,border=0,border_color=0):
+        super().__init__(fill,border,border_color)
+        self.points = points
+    
+    def draw(self):
+        if self.fill != None:
+            py.draw.polygon(WIN, self.fill, self.points)
+        
+        if self.border > 0:
+            py.draw.polygon(WIN, self.border_color, self.points, self.border)
+
+
+class Arc(Shape):
     def __init__(self,x,y,width,height,fill,border=0,thick=0):
-        super().__init__(fill,border,thick)
+        super().__init__(x,y,fill,border,thick)
         self.width = width
         self.height = height
 
+class Line(Shape):
+    def __init__(self,x,y,width,height,fill,border=0,thick=0):
+        super().__init__(x,y,fill,border,thick)
+        self.width = width
+        self.height = height
+
+class Button():
+    def __init__(self):
+        pass
+
+
+shapes = [Rect(5,5,10,10,(255,0,0), 2, (0,255,0))]
 
 def update():
-    WIN.fill(BLUE)
+    WIN.fill((WHITE))
+    py.draw.line(WIN, BLACK, (0, 640), (1080,640))
+
+    for shape in shapes:
+        shape.draw()
+
 
 while run:
     
