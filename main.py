d4806369d69ee054587798adf585ec2b87cfa66c
LucasJanssen <lucasjanssen2010@gmail.com>
2025-11-06 11:01:57 -0600

    fixed drawing, working on rectangle making

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ WIN = py.display.set_mode((WIDTH, HEIGHT))
 
 clock = py.time.Clock()
 
+sans_code = py.font.Font("SansCode.ttf", 20)
+
 WHITE = (255,255,255)
 BLACK = (0, 0, 0)
 BLACK = (0, 0, 0)
@@ -111,37 +113,70 @@ class Button():
         WIN.blit(self.img, self.rect)
     
     def check_click(self, mouse_pos):
-        if self.collidepoint(mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
             self.func
 
 shapes = []
+preview_shape = None
 buttons = []
+
 
 draw = False
 mouse_pos = ()
+brush_size = 5
+
+background = WHITE
+
+#Brush size text
+brush_size_label_text = sans_code.render("Brush Size", True, BLACK)
+brush_size_label_rect = brush_size_label_text.get_rect(midleft = (5,660))
+
+#Brush size number
+brush_size_text = sans_code.render(str(brush_size), True, BLACK)
+brush_size_rect = brush_size_text.get_rect(center = (65,690))
+
+#Rect button
+
+def draw_rect_preview():
+    print("hi")
+
+buttons.append(Button(200,645,"mario.jpg", draw_rect_preview))
+
+
 
 def update():
 
     global draw, shapes, mouse_pos
 
-    WIN.fill((WHITE))
-    py.draw.line(WIN, BLACK, (0, 640), (1080,640))
+    WIN.fill((background))
 
     if py.mouse.get_pressed()[0] and not draw:
         mouse_pos = py.mouse.get_pos()
         if mouse_pos[1] < 640:
             draw = True
+            shapes.append(Circle(mouse_pos, brush_size / 2,BLACK))
     
     if draw:
         cur_mouse_pos = py.mouse.get_pos()
-        shapes.append(Line(mouse_pos, cur_mouse_pos, BLACK, 10))
+        shapes.append(Line(mouse_pos, cur_mouse_pos, BLACK, brush_size))
+        shapes.append(Circle(cur_mouse_pos, brush_size / 2,BLACK))
         mouse_pos = cur_mouse_pos
         if not py.mouse.get_pressed()[0]:
-            shapes.append(Line(mouse_pos, py.mouse.get_pos(), BLACK, 10))
+            shapes.append(Line(mouse_pos, py.mouse.get_pos(), BLACK, brush_size))
+            shapes.append(Circle(py.mouse.get_pos(), brush_size / 2,BLACK))
             draw = False
 
     for shape in shapes:
         shape.draw()
+    
+    py.draw.rect(WIN, WHITE, (0, 640, 1080, 80))
+    py.draw.line(WIN, BLACK, (0, 640), (1080,640))
+    WIN.blit(brush_size_label_text, brush_size_label_rect)
+    WIN.blit(brush_size_text, brush_size_rect)
+
+    for button in buttons:
+        button.check_click(py.mouse.get_pos)
+        button.draw()
 
 
 while run:
@@ -158,6 +193,16 @@ while run:
                 mouse_pos = py.mouse.get_pos()
                 for button in buttons:
                     button.check_click(mouse_pos)
+        
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_PERIOD and brush_size < 100:
+                brush_size += 1
+                brush_size_text = sans_code.render(str(brush_size), True, BLACK)
+                brush_size_rect = brush_size_text.get_rect(center = (65,690))
+            if event.key == py.K_COMMA and brush_size > 0:
+                brush_size -= 1
+                brush_size_text = sans_code.render(str(brush_size), True, BLACK)
+                brush_size_rect = brush_size_text.get_rect(center = (65,690))
                 
     
     update()
